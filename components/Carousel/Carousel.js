fea00f1501f32af01cacf9431b5d44eca8900ac5
Chase Wex <clapingmanly@hotmail.com>
2019-04-12 13:25:49 -0400

    Added an auto switch after 10s
    Added logic to prevent user from cyleling through the images while the transition is still running.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -10,20 +10,28 @@ class Carousel {
         this.buttonLeft.addEventListener("click",()=> this.onclick(-1));
         this.element.addEventListener("mouseenter", ()=>this.onmousehover(1));
         this.element.addEventListener("mouseleave", ()=>this.onmousehover(0));
+        this.allowchange = true;
     }
     onmousehover(enter)
     {
         this.buttonLeft.classList.toggle("active-img")
         this.buttonRight.classList.toggle("active-img")
+        clearTimeout(inter);
+        inter = setTimeout(()=>this.onclick(1),10000);
     }
-
     onclick(dir)
     {
+        if(!this.allowchange) return;
         this.images.forEach(x=> x.classList.remove("active-img"));
         this.curent = (this.curent + ([...this.images].length+dir))%[...this.images].length;
         this.images[this.curent].classList.add("active-img")
-        console.log(this.curent);
+        //stop the user from clicking through these quickly
+        this.allowchange = false;
+        setTimeout(()=> this.allowchange = true, 1500)
     }
 }
 
-new Carousel(document.querySelector(".carousel"));
+var car = new Carousel(document.querySelector(".carousel"));
+var inter =  setTimeout(()=>
+    car.onclick(1)
+,10000)
